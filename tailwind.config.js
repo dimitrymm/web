20de4ce651c6af8f6ae19e8de9b1bf726108a5a9
Dimitry Machado Marinho <dimitrymmarinho@gmail.com>
2022-05-02 23:45:36 -0300

    Criação Push Projeto Widget

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/* It's telling the plugin what files to look at. */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors:{
+        brand:{
+          500:'#8257e6',
+        }
+      }
+
+    },
+  },
+  plugins: [],
+}
